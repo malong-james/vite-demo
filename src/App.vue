b781d024954d76c873752ed7655451f45e5e07f2
malong-james <crazypeppa@outlook.com>
2023-12-15 17:14:42 +0800

    feat : 布局完成 x2

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,14 @@
       <el-button type="primary">增加</el-button>
     </div>
     <!-- table -->
-    <el-table :data="tableData" style="width: 100%">
+    <el-table 
+    ref="multipleTableRef"
+    :data="tableData"
+    style="width: 100%"
+    @selection-change="handleSelectionChange"
+    border>
+
+    <el-table-column type="selection" width="55" />
     <el-table-column fixed prop="date" label="Date" width="150" />
     <el-table-column prop="name" label="Name" width="120" />
     <el-table-column prop="state" label="State" width="120" />
@@ -69,10 +76,18 @@
     zip: 'CA 90036',
     tag: 'Office',
   },]);
+
+  let multipleSelection = ref([])
   /*方法*/
   const handleRowClick = () =>{
     console.log('click')
   }
+  const handleSelectionChange = (val) => {
+  multipleSelection.value = val
+  console.log(val);
+}
+
+  
 
 
 </script>
@@ -85,5 +100,16 @@
   top: 50%;
   left: 50%;
   transform: translate(-50%,-50%);
+}
+.title{
+  text-align: center;
+}
+.query-box{
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+.el-input{
+  width: 200px;
 }
 </style>
